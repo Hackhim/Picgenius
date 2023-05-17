93987b16cad87d09dbf130f2d8808102f430c37f
JeremyLP <jeremyleprunenec.pro@gmail.com>
2023-05-17 21:33:21 +0200

    chore: Update structure

--- picgenius/models/__init__.py
+++
@@ -1,6 +0,0 @@
-"""Shortcuts the imports."""
-from .watermark import Watermark, Textbox
-from .template import Template, TemplateElement, TemplateImageElement
-from .video_settings import VideoSettings
-from .product import Product, Design
-from .product_type import ProductType, Format
