1635192037c60fa106e7bae068b6a9715aa0c0eb
JeremyLP <jeremyleprunenec.pro@gmail.com>
2023-05-18 11:33:01 +0200

    feat: Update logging messages

--- src/picgenius/controller.py
+++ src/picgenius/controller.py
@@ -54,8 +54,8 @@ class Controller:
     def process_product_generation(self, product: Product, output_dir: str):
         """Process the generation of all assets for the given product."""
 
-        self.logger.info("[%s] Start product all assets generation", product.name)
-        self.logger.info("[%s] output directory: %s", product.name, output_dir)
+        self.logger.info("(%s) Start product all assets generation", product.name)
+        self.logger.info("(%s) output directory: %s", product.name, output_dir)
         count_formats = len(product.type.formats)
         count_templates = len(product.type.templates)
         ProductRenderer.generate_formatted_designs(
@@ -65,46 +65,59 @@ class Controller:
             product, output_dir, max_threads=count_templates
         )
         ProductRenderer.generate_video(product, output_dir)
-        self.logger.info("[%s] All assets generation done", product.name)
+        self.logger.info("(%s) All assets generation done", product.name)
 
     def generate_products_templates(
         self, output_dir: str, template_name: Optional[str] = None
     ):
         """Generate products templates."""
         # TODO: Add generation of specified template
+        self.log_found_products(self.products)
         for product in self.products:
-            self.logger.info("[%s] Start templates generation", product.name)
-            self.logger.info("[%s] output directory: %s", product.name, output_dir)
+            self.logger.info("(%s) Start templates generation", product.name)
+            self.logger.info(
+                "(%s) output directory: %s",
+                product.name,
+                os.path.join(output_dir, product.name),
+            )
             ProductRenderer.generate_templates(product, output_dir)
-            self.logger.info("[%s] Templates generation done", product.name)
+            self.logger.info("(%s) Templates generation done", product.name)
 
     def generate_products_video(self, output_dir: str):
         """Generate products video."""
         for product in self.products:
-            self.logger.info("[%s] Start video generation", product.name)
-            self.logger.info("[%s] output directory: %s", product.name, output_dir)
+            self.logger.info("(%s) Start video generation", product.name)
+            self.logger.info(
+                "(%s) output directory: %s",
+                product.name,
+                os.path.join(output_dir, product.name),
+            )
             ProductRenderer.generate_video(product, output_dir)
-            self.logger.info("[%s] Video generation done", product.name)
+            self.logger.info("(%s) Video generation done", product.name)
 
     def generate_products_formatted_designs(self, output_dir: str):
         """Generate products formatted designs."""
         for product in self.products:
-            self.logger.info("[%s] Start formatted designs generation", product.name)
-            self.logger.info("[%s] output directory: %s", product.name, output_dir)
+            self.logger.info("(%s) Start formatted designs generation", product.name)
+            self.logger.info(
+                "(%s) output directory: %s",
+                product.name,
+                os.path.join(output_dir, product.name),
+            )
             ProductRenderer.generate_formatted_designs(product, output_dir)
-            self.logger.info("[%s] Formatted designs generation done", product.name)
+            self.logger.info("(%s) Formatted designs generation done", product.name)
 
     # TODO: add design upscaling for given product
     # def generate_product_upscaled_designs(self, output_dir: str, scale: int):
     #    """Generate upscaled designs."""
     #    for product in self.products:
     #        self.logger.info(
-    #            "[%s] Start x%d upscaled designs generation", product.name, scale
+    #            "(%s) Start x%d upscaled designs generation", product.name, scale
     #        )
-    #        self.logger.info("[%s] output directory: %s", product.name, output_dir)
+    #        self.logger.info("(%s) output directory: %s", product.name, output_dir)
     #        ProductRenderer.generate_formatted_designs(product, output_dir)
     #        self.logger.info(
-    #            "[%s] x%s upscaled designs generation done", product.name, scale
+    #            "(%s) x%s upscaled designs generation done", product.name, scale
     #        )
 
     def upscale_designs(self, output_dir: str, scale: int, cpu: bool = False):
@@ -114,12 +127,29 @@ class Controller:
             for design_path in utils.find_image_file_paths(self.design_path)
         ]
         os.makedirs(output_dir, exist_ok=True)
+
+        self.log_found_designs(designs)
         for design in designs:
             upscaled_path = os.path.join(
                 output_dir, f"{design.name}-x{scale}-upscaled.jpg"
             )
-            self.logger.info("[%s] Start x%d upscale", design.name, scale)
-            self.logger.info("[%s] output: %s", design.name, upscaled_path)
+            self.logger.info("(%s) Start x%d upscale", design.name, scale)
+            self.logger.info("(%s) output: %s", design.name, upscaled_path)
             upscaled_design = DesignRenderer.upscale_design(design, scale, cpu=cpu)
             upscaled_design.save(upscaled_path)
-            self.logger.info("[%s] x%s upscale done", design.name, scale)
+            self.logger.info("(%s) x%s upscale done", design.name, scale)
+
+    def log_found_designs(self, designs: list[Design]):
+        """Log found designs."""
+        self.logger.info("Found %d designs in %s", len(designs), self.design_path)
+        for design in designs:
+            self.logger.info("\t%s (%s)", design.path, design.name)
+
+    def log_found_products(self, products: list[Product]):
+        """Log found products."""
+        self.logger.info("Found %d products in %s", len(products), self.design_path)
+        for product in products:
+            self.logger.info("(%s)", product.name)
+            for design in product.designs:
+                self.logger.info("\t%s", design.path)
+            self.logger.info("\n")
